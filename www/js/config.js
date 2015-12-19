7fa9ed069e23e07f9504bd9f8cb3e6a7880c7f76
PortcullisYEG <dpenny17@gmail.com>
2015-12-19 11:44:22 -0700

    initial build

---
+++ www/js/config.js
@@ -0,0 +1,6 @@
+/* URL for the WordPress JSON API */
+var API_BASE_URL = "http://westerncup.com/";
+
+/* ID numbers for the PushWoosh messaging system */
+var PUSHWOOSH_ID = "7395D-5C79C";
+var GCM_PROJECT_NUMBER = "517748609686";
